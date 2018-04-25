3a45bbf43eb5cac6a13b80d1c2a246c15b15c433
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-04-25 21:42:11 +0100

    Terminal Hacker - Lecture 15 completed but not understood.
    I'll be back.

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -6,6 +6,14 @@ public class Hacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        //ShowMainMenu(words);
+        ShowMainMenu("Hello Ben");
+    }
+
+    void ShowMainMenu(string greeting) // The parameter is used so that it is used.
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting); // It knows greeting is a string because of the string in the above line.
         Terminal.WriteLine("Welcome to Terminal Hacker, human.");
         Terminal.WriteLine("Press 1 An MPs email passwords");
         Terminal.WriteLine("Press 2 Brenda's laptop");
