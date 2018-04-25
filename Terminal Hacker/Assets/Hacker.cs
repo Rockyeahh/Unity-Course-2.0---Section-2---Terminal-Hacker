39a8e02aa7330a966a0fd3e2cd1b90064b2275eb
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-04-25 17:56:40 +0100

    Terminal Hacker - Lecture 12 completed.
    Simple string stuff.

---
+++ Terminal Hacker/Assets/Hacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terminal.WriteLine("Welcome to Terminal Hacker, human.");
+        Terminal.WriteLine("Press 1 An MPs email passwords");
+        Terminal.WriteLine("Press 2 Brenda's laptop");
+        Terminal.WriteLine("Press 3 Money Corp Server");
+        Terminal.WriteLine("Choose a target:");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
