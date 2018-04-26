9f05daf4f7f3d6623b2e1cc76a60d63ec15775f8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-04-26 21:34:52 +0100

    Terminal Hacker - Lecture 24 is done.
    It had a challenge that I had try in order to complete. I messed it up slightly and watched a bit of the answer in order to fix my code.

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -8,7 +8,8 @@ public class Hacker : MonoBehaviour {
     // Game state.
     int level;
     enum Screen {MainMenu, Password, Win};
-    Screen currentScreen = Screen.MainMenu; // Declares a variable to use in the class. Then it intializes it with a new value by using Screen.MainMenu, so that it starts at the main menu.
+    Screen currentScreen; // Declares a variable to use in the class.
+    string password;
 
 	void Start () {
         ShowMainMenu();
@@ -38,6 +39,9 @@ public class Hacker : MonoBehaviour {
         else if (currentScreen == Screen.MainMenu)
         {
             RunMainMenu(input);
+        } else if (currentScreen == Screen.Password)
+        {
+            Password(input);
         }
       
     }
@@ -48,11 +52,15 @@ public class Hacker : MonoBehaviour {
         {
             level = 1;
             StartGame();
+            password = "Mistress1";
+            Terminal.WriteLine("Hint: stressiM1");
         }
         else if (input == "2")
         {
             level = 2;
             StartGame();
+            password = "Queen Of The Fools";
+            Terminal.WriteLine("Hint: sloof heT fuQeen O");
         }
         else if (input == "3")
         {
@@ -69,11 +77,24 @@ public class Hacker : MonoBehaviour {
         }
     }
 
-    private void StartGame()
+    void StartGame()
     {
         // He placed currentScreen = Screen.Password; here, which makes it so that you don't need repeated code above.
         currentScreen = Screen.Password;
         Terminal.WriteLine("You have chosen level " + level);
         Terminal.WriteLine("Please enter your password: ");
     }
+
+    void Password(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("Congratulations, Dave.");
+        } else
+        {
+            Terminal.WriteLine("Incorrect. Try again, Dave.");
+            // Could call Start Game again from here.
+        }
+    }
+
 }
